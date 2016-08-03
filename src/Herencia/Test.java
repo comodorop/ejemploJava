fcef1efa5256603c1850a2ee816e381c945ea922
pablo temporal <pablo temporal@Daniel>
2016-08-03 15:19:28 -0500

    Habilitar, deshabilitar, radios, check, jpassword field

---
+++ src/Herencia/Test.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Herencia;
+
+/**
+ *
+ * @author pablo temporal
+ */
+public class Test {
+    public static void main(String[] args) {
+        Persona p = new Persona("pjgt", "pjgt", "pjgt", "pjgt");
+    }
+}
